7350f521965e037f306f17cfd2af5a2b3db9cc92
Nesteruk Sergey <nesterus@protonmail.com>
2017-10-21 13:43:49 +0300

    redirect URI changed

--- Authorization/Server (PHP)/php/vk_settings.php
+++ Authorization/Server (PHP)/php/vk_settings.php
@@ -3,7 +3,7 @@
 $settings = [
     'client_id' => "6217072", // VKSETTINGS application id
     'app_key' => "Yw4uHVgjqisyTGzW18bM", // VKSETTINGS application secret key
-    'redirect_uri' => "http://nesterus.h1n.ru/",  // must be checked with trusted redirect URL specified in VKSETTINGS application settings panel
+    'redirect_uri' => "http://nesterus.h1n.ru/VK/Authorization/Server%20(PHP)/",  // must be checked with trusted redirect URL specified in VKSETTINGS application settings panel
     'v' => 5.68
 ];
 
